347763f25c455e22020317d9e452bbc43687c7e5
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-07-26 23:45:28 +1000

    Add jiti, pglite, crypto, jose and uuid

--- auth/nitro.config.ts
+++ auth/nitro.config.ts
@@ -1,5 +1,18 @@
 import { defineConfig } from "nitro"
 
 export default defineConfig({
-  serverDir: './server',
+    serverDir: './server',
+    experimental: {
+        database: true
+    },
+    database: {
+        default: {
+            connector: "pglite",
+            options: {
+                // Persists data to a local file directory inside your project
+                dataDir: './.data/credential' 
+            }
+        }
+    },
+    plugins: ['./server/plugins/db-init.ts']
 });
