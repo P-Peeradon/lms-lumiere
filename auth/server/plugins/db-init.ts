398ac01f31e859c377c848cdff089c050d6b759c
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-07-27 00:43:53 +1000

    Reimplement database and test query; Also, credential helper function.

--- auth/server/plugins/db-init.ts
+++ auth/server/plugins/db-init.ts
@@ -12,17 +12,27 @@ export default definePlugin(async (nitroApp: NitroApp) => {
     // Execute DDL statement to ensure your table exists
     // CREDENTIAL(shadow_id, hashed_password, hashed_username, role);
     await db.sql`
+      DO $$ 
+      BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'lms_role') THEN
+          CREATE TYPE LMS_Role as ENUM('student', 'instructor', 'faculty_admin', 'central_admin', 'system_admin');
+        END IF;
+      END $$;
+    `;
+
+
+    await db.sql`
         CREATE TABLE IF NOT EXISTS credentials (
         shadow_id CHAR(9) PRIMARY KEY,
         hashed_username TEXT UNIQUE NOT NULL,
         hashed_password TEXT NOT NULL,
-        user_role ENUM('student', 'instructor', 'faculty_admin', 'central_admin', 'system_admin') NOT NULL,
+        user_role LMS_Role NOT NULL,
         mfa BOOLEAN NOT NULL
       );
     `;
 
     await db.sql`
-      CREATE INDEX username_idx 
+      CREATE INDEX IF NOT EXISTS username_idx 
       ON credentials (hashed_username);
     `
     console.log('✅ Database schema ready.');
