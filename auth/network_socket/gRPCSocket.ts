38187cd391727124b24f01375e09e2a54fa9b99a
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-08-05 23:13:31 +1000

    define the client.

--- auth/network_socket/gRPCSocket.ts
+++ auth/network_socket/gRPCSocket.ts
@@ -1,9 +1,36 @@
-import grpc from '@grpc/grpc-js';
+import grpc, { ChannelCredentials } from '@grpc/grpc-js';
 import protoLoader from '@grpc/proto-loader';
+import path, { resolve } from 'node:path';
 
-const pkgDef = protoLoader.loadSync('d:/lms-lumiere/the-identity/Protos/identity.proto', {});
-const grpcObj = grpc.loadPackageDefinition(pkgDef) as any;
-const client = new grpcObj.identity.IdentityService('localhost:7120', grpc.credentials.createInsecure());
-// If TLS, use grpc.credentials.createSsl(...)
+const identityProto = path.resolve(__dirname, "..", "..", "the-identity", "Protos", "identity.proto");
+
+const packageDef = protoLoader.loadSync(identityProto, {
+    keepCase: false,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+});
+const proto: any = grpc.loadPackageDefinition(packageDef).identity;
+
+const creds: ChannelCredentials = grpc.credentials.createSsl();
+const client = proto.IdentityService("localhost:5120", creds)
+
+client.encryptedPII = function encryptedPII(
+    client: any,
+    req: Record<string, any>,
+    options?: { bearerToken?: string }
+): Promise<Record<string, any>> {
+    return new Promise((resolve, reject) => {
+        const metadata = new grpc.Metadata();
+        if (options?.bearerToken) metadata.add("authorization", `Bearer ${options.bearerToken}`);
+
+        // gRPC method signature: client.decryptPII(request, metadata?, callback)
+        client.encryptedPII(req, metadata, (err: any, res: any) => {
+            if (err) return reject(err);
+            resolve(res);
+        });
+    })
+}
 
 export default client;
