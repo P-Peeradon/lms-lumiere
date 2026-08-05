14ce906ae0d71c2462972fb8d22cf748cab436b9
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-08-05 22:13:15 +1000

    Edit verify token and define standard for event.response (NO use of HTTPResponse)

--- auth/server/auth/v1/(authz)/token/verify.post.ts
+++ auth/server/auth/v1/(authz)/token/verify.post.ts
@@ -1,4 +1,8 @@
-import { defineHandler, type H3Event } from "nitro";
+import AuthHelper from "#helper/AuthHelper.ts";
+import type { JWEPayload } from "#helper/interface.ts";
+import { defineHandler, HTTPError, type H3Event } from "nitro";
+import { useRuntimeConfig } from "nitro/runtime-config";
+import { ShadowID } from "#helper/interface.ts"
 
 /* 
 {
@@ -7,17 +11,49 @@ import { defineHandler, type H3Event } from "nitro";
   "iat": 1721970000,
   "exp": 1721973600,
 
-  "shadow_id": "opaque-uuid",
+  "shadow_id": "X8054061K",
   "role": "faculty_admin",
-  "scope": ["faculty.identity.read"],
-  "tenant": "university_enterprise",
+  "tenant": University,
 
   "session_id": "uuid",
   "device_id": "opaque-device-id",
   "mfa": true
 }
 */
+export default defineHandler(async (event: H3Event) => {
+    const { jweSecret } = useRuntimeConfig();
+    const token = event.req.headers.get("authorization")?.replace("Bearer ", "") ?? "";
 
-export default defineHandler((event: H3Event) => {
+    try {
+        const payload: JWEPayload = await AuthHelper.decryptToken(token, jweSecret);
 
+        const { role, shadow_id, iss, aud, tenant, iat, exp } = payload;
+        let shadowID: ShadowID;
+
+        try {
+            shadowID = ShadowID.parseShadowID(shadow_id as string);
+        } catch {
+            throw new HTTPError("Failure to parse the ShadowID", {
+                status: 422,
+                statusText: "Unprocessable Entity"
+            })
+        }
+
+        event.res.status = 200;
+        return {
+            shadowID,
+            role,
+            iss,
+            aud,
+            tenant,
+            iat,
+            exp
+        } as JWEPayload; // For attaching to the request header of other api request.
+    } catch (error) {
+        throw new HTTPError({ 
+            statusCode: 401, 
+            message: "Invalid token",
+            statusText: "Unauthorized"
+        });
+    }
 });
