a36c63462d409413d71ece2e6dc8c9896add1a46
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-08-08 20:54:00 +1000

    Unhash username for easily search.

--- auth/server/plugins/db-init.ts
+++ auth/server/plugins/db-init.ts
@@ -4,39 +4,39 @@ import { definePlugin } from 'nitro'
 import type { NitroApp } from 'nitro/types';
 
 export default definePlugin(async (nitroApp: NitroApp) => {
-  const db = useDatabase();
+    const db = useDatabase();
 
-  console.log('⚡ Initialising database schema...');
+    console.log('⚡ Initialising database schema...');
 
-  try {
-    // Execute DDL statement to ensure your table exists
-    // CREDENTIAL(shadow_id, hashed_password, hashed_username, role);
-    await db.sql`
-      DO $$ 
-      BEGIN
-        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'lms_role') THEN
-          CREATE TYPE LMS_Role as ENUM('student', 'instructor', 'faculty_admin', 'central_admin', 'system_admin');
-        END IF;
-      END $$;
-    `;
+    try {
+        // Execute DDL statement to ensure your table exists
+        // CREDENTIAL(shadow_id, hashed_password, hashed_username, role);
+        await db.sql`
+        DO $$ 
+        BEGIN
+            IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'lms_role') THEN
+            CREATE TYPE LMS_Role as ENUM('student', 'instructor', 'faculty_admin', 'central_admin', 'system_admin');
+            END IF;
+        END $$;
+        `;
 
 
-    await db.sql`
-        CREATE TABLE IF NOT EXISTS credentials (
-        shadow_id CHAR(9) PRIMARY KEY,
-        username TEXT UNIQUE NOT NULL,
-        hashed_password TEXT NOT NULL,
-        user_role LMS_Role NOT NULL,
-        mfa BOOLEAN NOT NULL
-      );
-    `;
+        await db.sql`
+            CREATE TABLE IF NOT EXISTS credentials (
+            shadow_id CHAR(9) PRIMARY KEY,
+            username TEXT UNIQUE NOT NULL,
+            hashed_password TEXT NOT NULL,
+            user_role LMS_Role NOT NULL,
+            mfa BOOLEAN NOT NULL
+        );
+        `;
 
-    await db.sql`
-      CREATE INDEX IF NOT EXISTS username_idx 
-      ON credentials (hashed_username);
-    `
-    console.log('✅ Database schema ready.');
-  } catch (error) {
-    console.error('❌ Failed to initialise database schema:', error);
-  }
+        await db.sql`
+        CREATE INDEX IF NOT EXISTS username_idx 
+        ON credentials (username);
+        `
+        console.log('✅ Database schema ready.');
+    } catch (error) {
+        console.error('❌ Failed to initialise database schema:', error);
+    }
 });
